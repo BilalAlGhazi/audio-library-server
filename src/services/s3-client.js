38355c368600b9d53d8fc9881df3e7a71c8c6a9b
Bilal <balghazi@gmail.com>
2019-01-17 11:43:11 -0500

    Remove S3 info

--- src/services/s3-client.js
+++ src/services/s3-client.js
@@ -2,8 +2,8 @@ const s3 = require('s3');
 
 const client = s3.createClient({  
   s3Options: {
-    accessKeyId: 'AKIAJUJLVYUTFCYQ2UGA',
-    secretAccessKey: '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'
+    accessKeyId: '',
+    secretAccessKey: ''
   }
 });
 
